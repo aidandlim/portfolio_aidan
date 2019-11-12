401f49ea7c89de3c174edab40c53e1706ecd4355
Aidan Lim <aidandlim@gmail.com>
2019-11-11 17:25:29 -0800

    init

---
+++ src/components/template/app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const App = () => {
+	return (
+		<div className="app">
+			
+		</div> 
+	);
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './components/app/App';
+
+import App from './components/template/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
