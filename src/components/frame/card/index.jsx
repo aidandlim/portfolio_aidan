8e8d698abadb32f1cc58735eff7fd1456a766d9c
Aidan Lim <aidandlim@gmail.com>
2019-11-14 22:15:55 -0800

    update design

--- src/components/frame/card/index.jsx
+++ src/components/frame/card/index.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 
-import { FaCode, FaBookmark, FaDesktop } from "react-icons/fa";
+import { FaUser, FaUserFriends, FaCheck, FaCircleNotch } from 'react-icons/fa';
 
 import './index.css';
 
 const Card = (props) => {
 	return (
-		<div className='card'>
-			<img src={props.data.preview} alt='portfolio' />
+		<div className={props.data.isProgress ? 'card card-progress' : 'card'} >
+			{props.data.preview !== '' ? <img src={props.data.preview} alt='portfolio' /> : <div class='workinprogress'>Work in Progress...</div> }
 			<div className='card-detail'>
+				{ props.data.isPersonal ? <FaUser className='card-detail-user-icon' /> : <FaUserFriends className='card-detail-user-icon' /> }
+				{ props.data.isProgress ? <FaCircleNotch className='card-detail-progress-icon' /> : <FaCheck className='card-detail-progress-icon' /> }
 				<div className='card-detail-title'>{props.data.title}</div>
 				<div className='card-detail-description'>{props.data.description1}</div>
 				<div className='card-detail-description'>{props.data.description2}</div>
-				<div className='card-detail-link'>
-					<FaBookmark className='card-detail-link-icon'/>
-					<div className='card-detail-link-title'>Doc</div>
-				</div>
-				<div className='card-detail-link'>
-					<FaCode className='card-detail-link-icon'/>
-					<div className='card-detail-link-title'>Code</div>
-				</div>
-				<div className='card-detail-link'>
-					<FaDesktop className='card-detail-link-icon'/>
-					<div className='card-detail-link-title'>Demo</div>
+				<div className='card-detail-link-container'>
+					{props.data.page !== '' ? <div className='card-detail-link' onClick={ () => window.open(props.data.page, '_black') }>Document</div> : ''}
+					{props.data.code !== '' ? <div className='card-detail-link' onClick={ () => window.open(props.data.code, '_black') }>Source Code</div> : ''}
+					{props.data.site !== '' ? <div className='card-detail-link' onClick={ () => window.open(props.data.site, '_black') }>Demo Site</div> : ''}
 				</div>
 			</div>
 		</div>
--- src/components/frame/portfolio/index.jsx
+++ src/components/frame/portfolio/index.jsx
@@ -7,14 +7,38 @@ import './index.css';
 const Portfolio = (props) => {
 	const array = [
 		{
+			isPersonal: 1,
+			isProgress: 0,
+			title: 'Aidan\'s',
+			description1: '- A responsive Single Page Application(SPA) using React',
+			description2: '- A simple web application for introducing myself',
+			preview: '',
+			code: 'https://github.com/aidandlim/portfolio_aidan',
+			page: 'https://aidandlim.github.io/portfolio_aidan/',
+			site: 'https://www.aidandlim.com/',
+		},
+		{
+			isPersonal: 1,
+			isProgress: 0,
 			title: '#Camagru',
-			description1: 'description1',
-			description2: 'description2',
+			description1: '- A responsive Single Page Application(SPA) using React & Java',
+			description2: '- A Social Network Service(SNS) for sharing pictures like Instagram',
 			preview: 'https://github.com/aidandlim/portfolio_camagru/blob/master/PREVIEW.gif?raw=true',
-			code: 'https://github.com/aidandlim/portfolio_camagru',
+			code: 'https://github.com/aidandlim/portfolio_camagru/',
 			page: 'https://aidandlim.github.io/portfolio_camagru/',
-			site: 'https://camagru.aidandlim.com',
-		}
+			site: 'https://camagru.aidandlim.com/',
+		},
+		{
+			isPersonal: 2,
+			isProgress: 1,
+			title: 'M@TCH@',
+			description1: '- A responsive Single Page Application(SPA) using React & Node',
+			description2: '- A Social Network Service(SNS) for matching dating partner like Tinder',
+			preview: '',
+			code: 'https://github.com/aidandlim/portfolio_matcha/',
+			page: 'https://aidandlim.github.io/portfolio_matcha/',
+			site: '',
+		},
 	]
 	return (
 		<div className='portfolio'>
@@ -31,9 +55,9 @@ const Portfolio = (props) => {
 				<h2>저의 최근 프로젝트들 입니다. 제 깃허브 페이지에서 모든 프로젝트를 확인하실 수 있습니다.</h2>
 			}
 			<div>
-				<Card data={array[0]} />
-				<Card data={array[0]} />
-				<Card data={array[0]} />
+				{array.map((data, index) => 
+					<Card data={data} key={index} />
+				)}
 			</div>
 		</div>
 	);
