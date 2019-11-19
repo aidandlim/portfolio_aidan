8d05c31795e329678345939a7da9995179e28209
Aidan Lim <aidandlim@gmail.com>
2019-11-19 13:26:52 -0800

    renewal

--- src/components/frame/about/index.jsx
+++ src/components/frame/about/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { FaMapMarkerAlt, FaPhoneSquare } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
+
 import './index.css';
 
 const About = (props) => {
@@ -24,6 +27,14 @@ const About = (props) => {
 				: 
 				<h1>안녕하세요. 저는 임동섭입니다. 만나서 반갑습니다.</h1>
 			}
+			<div className='about-info'>
+				<FaMapMarkerAlt className='about-info-icon' />
+				<div className='about-info-detail'>Fremont, CA, USA</div>
+				<IoMdMail className='about-info-icon' />
+				<div className='about-info-detail'>aidandlim@gmail.com</div>
+				<FaPhoneSquare className='about-info-icon' />
+				<div className='about-info-detail'>+1 (949) 426 - 5643</div>
+			</div>
 			{!props.lang 
 				? 
 				<div className='about-detail'>
@@ -38,7 +49,7 @@ const About = (props) => {
 				<div className='about-detail'>
 					제 자신을 한 문장으로 표현하라면, 저는 <b>"여행을 사랑하는 웹 & 앱 개발자"라고 표현할 것입니다</b>.<br />
 					...<br />
-					저는 비즈니스 컨설팅 기업의 솔루션개발사업부에서<br />
+					저는 한국에서 비즈니스 컨설팅 기업의 솔루션개발사업부에서<br />
 					<b>자바</b>와 <b>자바스크립트</b>, 그리고 <b>SQL</b>을 주로 활용하는 <b>풀스택 개발자</b>로써 3년 반동안 일했습니다.<br />
 					현재는 <font className='about-highlight' onClick={_handleHyperlinkToCollege}>#DeAnzaCollege</font>와 부트캠프 <font className='about-highlight' onClick={_handleHyperlinkToBootcamp}>#42SiliconValley</font>에서 컴퓨터 공학을 공부하고 있습니다.<br />
 					또한, 프론트엔드 개발자로써 <b>자바스크립트</b>를 기반으로 하는 <b>리액트</b>를 활용한 웹 & 앱 개발에 집중하고 있습니다.
--- src/components/function/card/index.jsx
+++ src/components/function/card/index.jsx
@@ -5,6 +5,7 @@ import { FaUser, FaUserFriends, FaCheck, FaCircleNotch } from 'react-icons/fa';
 import './index.css';
 
 const Card = (props) => {
+	
 	return (
 		<div className={props.data.isProgress ? 'card card-progress' : 'card'} >
 			<div className='card-header'>
@@ -13,12 +14,18 @@ const Card = (props) => {
 				<div className='card-title'>{props.data.title}</div>
 			</div>
 			<div className='card-body'>
-				<div className='card-description'>{props.data.description1}</div>
+				<div className='card-description'>{props.data.descriptionEN}</div>
+				{
+					props.data.language.map((lang, index) =>
+						<div className='card-language' key={index}>#{lang}</div>
+				)}
 			</div>
 			<div className='card-footer'>
-				{props.data.page !== '' ? <div className='card-link card-link-document' onClick={ () => window.open(props.data.page, '_black') }>Document</div> : ''}
-				{props.data.code !== '' ? <div className='card-link card-link-code' onClick={ () => window.open(props.data.code, '_black') }>Source Code</div> : ''}
-				{props.data.site !== '' ? <div className='card-link card-link-demo' onClick={ () => window.open(props.data.site, '_black') }>Demo Site</div> : ''}
+				<div className='card-link-container'>
+					{props.data.docs !== '' ? <div className='card-link card-link-docs' onClick={ () => window.open(props.data.docs, '_black') }>Document</div> : ''}
+					{props.data.code !== '' ? <div className='card-link card-link-code' onClick={ () => window.open(props.data.code, '_black') }>Source Code</div> : ''}
+					{props.data.demo !== '' ? <div className='card-link card-link-demo' onClick={ () => window.open(props.data.demo, '_black') }>Demo Site</div> : ''}
+				</div>
 			</div>
 		</div>
 	);
